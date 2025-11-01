1237117d49bd9321e197b2ef71c8a97f136221b4
rdgldkthu <rdg22@mails.tsinghua.edu.cn>
2025-11-01 14:23:36 +0900

    refactor(dsa): simplify code for week1-A

--- dsa/cpp/kundol/week1/1-A_2309.cpp
+++ dsa/cpp/kundol/week1/1-A_2309.cpp
@@ -1,8 +1,8 @@
+// Solve with combination
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int temp;
 int arr[9];
 
 int sum7(int arr[]) {
@@ -13,26 +13,21 @@ int sum7(int arr[]) {
 }
 
 int main(int argc, char **argv) {
-  for (int i = 0; i < 9; i++) {
-    cin >> temp;
-    arr[i] = temp;
-  }
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-  // brute-force
+  for (int i = 0; i < 9; i++)
+    cin >> arr[i];
+
   for (int i = 0; i < 8; i++) {
     swap(arr[i], arr[7]);
     for (int j = i + 1; j < 9; j++) {
-      if (j == 7) {
-        swap(arr[i], arr[8]);
-        if (sum7(arr) == 100)
-          break;
-        swap(arr[i], arr[8]);
-      } else {
-        swap(arr[j], arr[8]);
-        if (sum7(arr) == 100)
-          break;
-        swap(arr[j], arr[8]);
-      }
+      int idx = (j == 7) ? i : j;
+      swap(arr[idx], arr[8]);
+      if (sum7(arr) == 100)
+        break;
+      swap(arr[idx], arr[8]);
     }
     if (sum7(arr) == 100)
       break;
