d6fa3f38b82c82e8149c8e97aa789f3caa4fc4f9
rdgldkthu <rdg22@mails.tsinghua.edu.cn>
2025-11-01 14:28:29 +0900

    feat(dsa): solve week1-B

---
+++ dsa/cpp/kundol/week1/1-B_10808.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  map<char, int> alphabet;
+  for (int i = 97; i < 97 + 26; i++) {
+    alphabet[static_cast<char>(i)] = 0;
+  }
+
+  string input;
+  cin >> input;
+
+  for (const auto &ch : input)
+    alphabet[ch]++;
+
+  for (const auto &pair : alphabet)
+    cout << pair.second << " ";
+  cout << endl;
+
+  return 0;
+}
